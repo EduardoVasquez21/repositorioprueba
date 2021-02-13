f13305334ea83bc13df2c002d81caa1a2351b59f
EduardoVasquez21 <78975314+EduardoVasquez21@users.noreply.github.com>
2021-02-13 08:17:24 -0600

    Creacion de proyecto

---
+++ Eduardo01/Eduardo01/NEGOCIO/ClsLogin.cs
@@ -0,0 +1,24 @@
+﻿using Eduardo01.DOMINIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eduardo01.NEGOCIO
+{
+    class ClsLogin
+    {
+       
+
+        public int acceso(Login log)
+        {
+            int estado = 0;
+            if (log.Usuario.Equals("Eduardo") && log.Password.Equals("123")) {
+                estado = 1;
+            }
+            
+
+                return estado;
+    }
+}

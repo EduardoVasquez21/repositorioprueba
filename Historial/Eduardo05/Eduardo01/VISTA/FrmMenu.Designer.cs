cf9d45892412755cd3504de09d1b6f8ac24cc672
EduardoVasquez21 <vl0815032020@unab.edu.sv>
2021-03-23 08:56:31 -0600

    .

---
+++ Historial/Eduardo05/Eduardo01/VISTA/FrmMenu.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace Eduardo01.VISTA
+{
+    partial class FrmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clickParaMasInformacionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.arreglosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.matematicasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.conexionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cRUDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acercaDeToolStripMenuItem,
+            this.arreglosToolStripMenuItem,
+            this.matematicasToolStripMenuItem,
+            this.conexionesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(493, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clickParaMasInformacionToolStripMenuItem});
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de";
+            this.acercaDeToolStripMenuItem.Click += new System.EventHandler(this.acercaDeToolStripMenuItem_Click);
+            // 
+            // clickParaMasInformacionToolStripMenuItem
+            // 
+            this.clickParaMasInformacionToolStripMenuItem.Name = "clickParaMasInformacionToolStripMenuItem";
+            this.clickParaMasInformacionToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.clickParaMasInformacionToolStripMenuItem.Text = "Click para mas informacion";
+            this.clickParaMasInformacionToolStripMenuItem.Click += new System.EventHandler(this.clickParaMasInformacionToolStripMenuItem_Click);
+            // 
+            // arreglosToolStripMenuItem
+            // 
+            this.arreglosToolStripMenuItem.Name = "arreglosToolStripMenuItem";
+            this.arreglosToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.arreglosToolStripMenuItem.Text = "Arreglos";
+            this.arreglosToolStripMenuItem.Click += new System.EventHandler(this.arreglosToolStripMenuItem_Click);
+            // 
+            // matematicasToolStripMenuItem
+            // 
+            this.matematicasToolStripMenuItem.Name = "matematicasToolStripMenuItem";
+            this.matematicasToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.matematicasToolStripMenuItem.Text = "Matematicas";
+            // 
+            // conexionesToolStripMenuItem
+            // 
+            this.conexionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cRUDToolStripMenuItem});
+            this.conexionesToolStripMenuItem.Name = "conexionesToolStripMenuItem";
+            this.conexionesToolStripMenuItem.Size = new System.Drawing.Size(96, 20);
+            this.conexionesToolStripMenuItem.Text = "ConexionesBD";
+            this.conexionesToolStripMenuItem.Click += new System.EventHandler(this.conexionesToolStripMenuItem_Click);
+            // 
+            // cRUDToolStripMenuItem
+            // 
+            this.cRUDToolStripMenuItem.Name = "cRUDToolStripMenuItem";
+            this.cRUDToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cRUDToolStripMenuItem.Text = "CRUD";
+            this.cRUDToolStripMenuItem.Click += new System.EventHandler(this.cRUDToolStripMenuItem_Click);
+            // 
+            // FrmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmMenu";
+            this.Text = "Menu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clickParaMasInformacionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem arreglosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem matematicasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem conexionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cRUDToolStripMenuItem;
+    }
+}
---
+++ Historial/Eduardo05/Eduardo01/VISTA/FrmMenu.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eduardo01.VISTA
+{
+    public partial class FrmMenu : Form
+    {
+        public FrmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void clickParaMasInformacionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmAcercade frm = new FrmAcercade();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void arreglosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmvistaUSUARIOvisual frm = new FrmvistaUSUARIOvisual();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void conexionesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cRUDToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmUsuarios frm = new FrmUsuarios();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+    }
+}
